10ea46862beefe411d05c9ecb29717eb240a812a
Manajit Halder <manajit.halder@gmail.com>
2024-06-11 21:37:59 +0530

    Minor changes in FundMe and PriceConverter

--- freecodecamp/storageFactory/contracts/FundMe.sol
+++ freecodecamp/storageFactory/contracts/FundMe.sol
@@ -5,14 +5,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract FuncMe {
-    uint256 public minimumUSD = 50;
+contract FundMe {
+    uint256 public constant MINIMUM_USD = 50;
 
     function fund() public payable {
+        // Want to be able to set a minimum fund amount in USD
         // Send some ETH to this contract?
-        require(msg.value >= minimumUSD, "Insufficient fund received");
+        require(msg.value >= 1e18, "Didn't send enough!");
     }
 
+    // Since getPrice will get the price from contract outside of this contract, we are going to need
+    // ABI and address of the external contract to interact with it.
     function getPrice() public {
         // ABI
         // contract address
