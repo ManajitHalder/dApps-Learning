bbdf7635fca3c14c43c71f5a471d6a3b02b1d62e
Manajit Halder <manajit.halder@gmail.com>
2024-06-12 22:36:30 +0530

    Added AggregatorV3Interface interface for getting ETH/USD value

--- freecodecamp/storageFactory/contracts/FundMe.sol
+++ freecodecamp/storageFactory/contracts/FundMe.sol
@@ -5,8 +5,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
     uint256 public constant MINIMUM_USD = 50;
+    AggregatorV3Interface internal priceFeed;
+
+    constructor() {
+
+        priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+    }
 
     function fund() public payable {
         // Want to be able to set a minimum fund amount in USD
@@ -18,8 +26,11 @@ contract FundMe {
     // ABI and address of the external contract to interact with it.
     function getPrice() public {
         // ABI
-        // contract address
+        // contract address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        (, int256 answer, , , ) = priceFeed.getRoundData();
     }
+
+    function getConversionRate() public {}
 
     function withdraw() public {}
 }
