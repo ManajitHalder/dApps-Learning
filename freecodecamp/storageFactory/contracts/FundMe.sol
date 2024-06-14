230dc0429fe2a932550a1d2d95798cb56cd848a4
Manajit Halder <manajit.halder@gmail.com>
2024-06-14 21:37:21 +0530

    final changes

--- freecodecamp/storageFactory/contracts/FundMe.sol
+++ freecodecamp/storageFactory/contracts/FundMe.sol
@@ -5,32 +5,62 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
+
+error NotOwner();
 
 contract FundMe {
-    uint256 public constant MINIMUM_USD = 50;
-    AggregatorV3Interface internal priceFeed;
+    using PriceConverter for uint256;
 
-    constructor() {
+    uint256 public constant MINIMUM_USD = 50 * 1e18;
+    address public immutable i_owner;
+   
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
 
-        priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+    constructor() {        
+        i_owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        // require(msg.sender == i_owner, "Only owner can call this function");
+        if (msg.sender != i_owner) {
+            revert NotOwner();
+        }
+        _;
     }
 
     function fund() public payable {
         // Want to be able to set a minimum fund amount in USD
         // Send some ETH to this contract?
-        require(msg.value >= 1e18, "Didn't send enough!");
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Didn't send enough!");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    // Since getPrice will get the price from contract outside of this contract, we are going to need
-    // ABI and address of the external contract to interact with it.
-    function getPrice() public {
-        // ABI
-        // contract address 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        (, int256 answer, , , ) = priceFeed.getRoundData();
+    function withdraw() public onlyOwner {
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        // reset the funders array
+        funders = new address[](0);
+
+        // // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+        // // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "send failed");
+        // call 
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "call failed");
     }
 
-    function getConversionRate() public {}
+    receive() external payable {
+        fund();
+    }
 
-    function withdraw() public {}
+    fallback() external payable {
+        fund();
+    }
 }
--- freecodecamp/storageFactory/contracts/PriceConverter.sol
+++ freecodecamp/storageFactory/contracts/PriceConverter.sol
@@ -1,6 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract PriceConverter {
-    
+import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+
+        // Returns value of 1 eth in USD and converts the result to ETH as wei before returning
+    function getPrice() internal view returns (uint256) {
+        /**
+        * Network: Sepolia
+        * Aggregator: ETH/USD
+        * Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        */
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (
+            /*uint80 roundId*/, 
+            int256 answer,
+            /*uint256 startedAt*/,
+            /*uint256 updatedAt*/,
+            /*uint80 answeredInRound*/
+        ) = priceFeed.latestRoundData();
+        // answer is price of ETH in USD
+        
+        return uint256(answer * 1e10); // 
+    }
+
+    function getVersion() internal view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
+    }
+
+    // Converts ETH (Wei) to USD and returns result in ETH as wei
+    function getConversionRate(uint256 ethAmountInWei /* ETH as WEI*/) internal view returns (uint256) {
+        uint256 weiInUSD = getPrice();
+        uint256 weiAmountInUSD = (weiInUSD * ethAmountInWei) / 1e18;
+
+        return weiAmountInUSD;
+    }
 }
