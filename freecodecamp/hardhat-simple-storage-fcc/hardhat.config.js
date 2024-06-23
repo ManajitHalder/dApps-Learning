e6fa68af07f4c7b256a1e8f44283a3f775daad9c
Manajit Halder <manajit.halder@gmail.com>
2024-06-23 22:45:36 +0530

    added sepolia testnet details

--- freecodecamp/hardhat-simple-storage-fcc/hardhat.config.js
+++ freecodecamp/hardhat-simple-storage-fcc/hardhat.config.js
@@ -1,6 +1,33 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners()
+
+    for (const account of accounts) {
+        console.log(account)
+    }
+})
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
-};
+    defaultNetwork: "hardhat",
+    solidity: "0.8.24",
+    networks: {
+        hardhat: {
+            chainId: 1337,
+        },
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 11155111,
+        },
+        localhost: {
+            url: "http://localhost:8545",
+            chainId: 31337,
+        },
+    },
+}
