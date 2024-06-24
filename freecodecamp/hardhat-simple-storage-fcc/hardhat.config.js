039cc465ede00c42cdd55fb314895b42e6b079e2
Manajit Halder <manajit.halder@gmail.com>
2024-06-24 16:27:15 +0530

    Deploying on sepolia network changes

--- freecodecamp/hardhat-simple-storage-fcc/hardhat.config.js
+++ freecodecamp/hardhat-simple-storage-fcc/hardhat.config.js
@@ -1,21 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
-
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-    const accounts = await hre.ethers.getSigners()
-
-    for (const account of accounts) {
-        console.log(account)
-    }
-})
+require("@nomicfoundation/hardhat-verify")
+require("./tasks/block-number")
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
     defaultNetwork: "hardhat",
-    solidity: "0.8.24",
     networks: {
         hardhat: {
             chainId: 1337,
@@ -29,5 +23,9 @@ module.exports = {
             url: "http://localhost:8545",
             chainId: 31337,
         },
+    },
+    solidity: "0.8.24",
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
     },
 }
--- freecodecamp/hardhat-simple-storage-fcc/scripts/deploy.js
+++ freecodecamp/hardhat-simple-storage-fcc/scripts/deploy.js
@@ -8,11 +8,48 @@ async function main() {
         )
         console.log("Deploying contract...")
         const simpleStorage = await SimpleStorageFactory.deploy()
-        // simpleStorage.deployed
 
-        // console.log(`Deployed contract to address: ${simpleStorage.address}`)
+        // console.log(network.config)
+
+        // Deploy to hardhat network (sepolia)
+        if (
+            network.config.chainId === 11155111 &&
+            process.env.ETHERSCAN_API_KEY
+        ) {
+            console.log("Waiting for block transactions ...")
+            await simpleStorage.deploymentTransaction().wait(6)
+            await verify(simpleStorage.target, [])
+        }
+
+        // Interact with SimpleStorage contract
+        const currentValue = await simpleStorage.retrieve()
+        console.log(`Current value is ${currentValue}`)
+        
+
+        // Update current value
+        const transactionResponse = await simpleStorage.store(423)
+        await transactionResponse.wait(1)
+        const updatedValue = await simpleStorage.retrieve()
+        console.log(`Updated value is ${updatedValue}`)
     } catch (error) {
         console.error("Error deploying contract:", error)
+    }
+}
+
+// async function verify(contractAddress, args) {
+const verify = async (contractAddress, args) => {
+    console.log("Verifying contract...")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already verified")
+        } else {
+            console.log(e)
+        }
     }
 }
 
