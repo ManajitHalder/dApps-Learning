6679c0367d2e26048c79f4cd164e5b5de5aa23e1
Manajit Halder <manajit.halder@gmail.com>
2024-07-02 21:44:41 +0530

    Added ethers locally and fund function

--- freecodecamp/html-fund-me-fcc/README.md
+++ freecodecamp/html-fund-me-fcc/README.md
@@ -1,9 +1,36 @@
 # Frontend for FundMe Contract using html and javascript
 
-Create index.html file
+## Create index.html file
 
 Add ! content
 
 Connect to Metamask wallet
 
-Write javascript code inside index.html inside the body tag
+## Write javascript code inside index.html inside the body tag
+
+## Add .prettierrc and install prettier
+
+```
+yarn add --dev prettier
+```
+
+Changes in .prettierrc file:
+
+```
+{
+    "tabWidth": 4,
+    "useTabs": false,
+    "semi": false,
+    "singleQuote": false
+}
+```
+
+## Fund
+
+To be able to send a transaction using fund we need a provider / connection to the blockchain
+
+signer / wallet / someone with some gas,
+
+contract that we are interacting with, and to get the contract we need the ABI and the address.
+
+Copy ethers code from https://cdn.ethers.io/lib/ethers-5.1.esm.min.js to a file with same name in your project. Refer url *[https://docs.ethers.org/v5/getting-started/](https://docs.ethers.org/v5/getting-started/)* for details.
--- freecodecamp/html-fund-me-fcc/index.js
+++ freecodecamp/html-fund-me-fcc/index.js
@@ -1,9 +1,24 @@
+import { ethers } from "./ethers-5.2.esm.min.js"
+
+const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fundButton")
+
+connectButton.onclick = connect
+fundButton.onclick = fund
+
 async function connect() {
-  if (typeof window.ethereum !== 'undefined') {
-    await window.ethereum.request({ method: 'eth_requestAccounts' });
-    document.getElementById('connectButton').innerHTML = 'Connected!';
-  } else {
-    document.getElementById('connectButton').innerHTML =
-      'Please install metamask!!!';
-  }
+    if (typeof window.ethereum !== "undefined") {
+        try {
+            await window.ethereum.request({ method: "eth_requestAccounts" })
+        } catch (error) {
+            console.log(error)
+        }
+        connectButton.innerHTML = "Connected"
+    } else {
+        connectButton.innerHTML = "Please install metamask!!!"
+    }
+}
+
+async function fund(ethAmount) {
+    console.log(`Funding with ${ethAmount}`)
 }
