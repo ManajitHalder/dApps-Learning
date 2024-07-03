5e2d4436154a0fb1d69722f6d358c84986a7cf9e
Manajit Halder <manajit.halder@gmail.com>
2024-07-03 22:15:19 +0530

    completed fund me html js changes

--- freecodecamp/html-fund-me-fcc/index.js
+++ freecodecamp/html-fund-me-fcc/index.js
@@ -1,10 +1,15 @@
-import { ethers } from "./ethers-5.2.esm.min.js"
+import { ethers } from "./ethers-5.6.esm.min.js"
+import { abi, contractAddress } from "./constants.js"
 
 const connectButton = document.getElementById("connectButton")
 const fundButton = document.getElementById("fundButton")
+const balanceButton = document.getElementById("balanceButton")
+const withdrawButton = document.getElementById("withdrawButton")
 
 connectButton.onclick = connect
 fundButton.onclick = fund
+balanceButton.onclick = getBalance
+withdrawButton.onclick = withdraw
 
 async function connect() {
     if (typeof window.ethereum !== "undefined") {
@@ -19,6 +24,91 @@ async function connect() {
     }
 }
 
-async function fund(ethAmount) {
+// async function connect() {
+//     if (typeof window.ethereum !== "undefined") {
+//         try {
+//             const provider = new ethers.providers.Web3Provider(window.ethereum)
+//             await provider.send("eth_requestAccounts", [])
+//             // await window.ethereum.request({ method: "eth_requestAccounts" })
+//         } catch (error) {
+//             console.log(error)
+//         }
+//         connectButton.innerHTML = "Connected"
+//     } else {
+//         connectButton.innerHTML = "Please install metamask!!!"
+//     }
+// }
+
+async function fund() {
+    const ethAmount = document.getElementById("ethAmount").value
     console.log(`Funding with ${ethAmount}`)
+    if (typeof window.ethereum !== "undefined") {
+        // provider / connection to the blockchain
+        // signer / wallet / someone with some gas
+        // contract that we are interacting with
+        // ^ ABI & Address
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            const transactionResponse = await contract.fund({
+                value: ethers.utils.parseEther(ethAmount),
+            })
+            // Wait for the transaction to finish
+            await listenForTransactionMine(transactionResponse, provider)
+        } catch (error) {
+            console.log(error)
+        }
+    } else {
+        fundButton.innerHTML = "Please install Metamask"
+    }
+}
+
+async function getBalance() {
+    if (typeof window.ethereum !== undefined) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        try {
+            const blalance = await provider.getBalance(contractAddress)
+            console.log("Balance: %s", ethers.utils.formatEther(blalance))
+        } catch (error) {
+            console.log(error)
+        }
+    } else {
+        balanceButton.innerHTML = "Please install Metamask"
+    }
+}
+
+async function withdraw() {
+    console.log("Withdrawing fund ...")
+    if (typeof window.ethereum !== undefined) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        await provider.send("eth_requestAccounts", [])
+        // const balance = await provider.getBalance(contractAddress)
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            const transactionResponse = await contract.withdraw()
+            await listenForTransactionMine(transactionResponse, provider)
+        } catch (error) {
+            console.log(error)
+        }
+    } else {
+        withdrawButton.innerHTML = "Please install Metamask"
+    }
+}
+
+function listenForTransactionMine(transactionResponse, provider) {
+    console.log(`Mining ${transactionResponse.hash}...`)
+    return new Promise((resolve, reject) => {
+        try {
+            provider.once(transactionResponse.hash, (transactionReceipt) => {
+                console.log(
+                    `Completed with ${transactionReceipt.confirmations} confirmations`,
+                )
+                resolve()
+            })
+        } catch (error) {
+            reject(error)
+        }
+    })
 }
