8d135729c2c5476a29cf2690c4e80d0a71ba100b
Manajit Halder <manajit.halder@gmail.com>
2024-07-20 23:10:00 +0530

    Created raffle frontend project

---
+++ freecodecamp/hardhat-lottery-fcc/frontend/pages/index.js
@@ -0,0 +1,21 @@
+import Head from "next/head"
+import Image from "next/image"
+import { Inter } from "next/font/google"
+import styles from "@/styles/Home.module.css"
+import ManualHeader from "../components/ManualHeader"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Smart Contract Raffle</title>
+        <meta name="description" content="Our Smart Contract Raffle" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ManualHeader />
+    </>
+  )
+}
