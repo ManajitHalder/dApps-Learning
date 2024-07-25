bf8b16c92617181fc1dfe39ec1217bffb9130410
Manajit Halder <manajit.halder@gmail.com>
2024-07-25 21:31:34 +0530

    Create hardhat defi (Aave) project with initial setup completed

---
+++ freecodecamp/hardhat-defi-fcc/smartcontract/README.md
@@ -0,0 +1,22 @@
+# Defi Aave
+
+Create folder smartcontract
+Create README.md file
+
+Install hardhat
+
+```
+yarn add --dev hardhat
+```
+
+Create a hardhat project and select "Create an empty hardhat.config.js"
+
+```
+yarn hardhat
+```
+
+Install devepdencies
+
+```
+yarn add --dev @nomiclabs/hardhat-ethers@npm:hardhat-deploy-ethers ethers @nomiclabs/hardhat-etherscan @nomiclabs/hardhat-waffle chai ethereum-waffle hardhat hardhat-contract-sizer hardhat-deploy hardhat-gas-reporter prettier prettier-plugin-solidity solhint solidity-coverage dotenv
+```
---
+++ freecodecamp/hardhat-defi-fcc/smartcontract/hardhat.config.js
@@ -0,0 +1,72 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-etherscan');
+require('hardhat-deploy');
+require('solidity-coverage');
+require('hardhat-gas-reporter');
+require('hardhat-contract-sizer');
+require('dotenv').config();
+require('@nomiclabs/hardhat-ethers');
+
+const SEPOLIA_RPC_URL =
+  process.env.SEPOLIA_RPC_URL ||
+  'https://eth-sepolia.g.alchemy.com/v2/P1KIhRkvzxF_FV_OTzkAHZYMLqdPTKCt';
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '';
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || '';
+const COINMARKET_API_KEY = process.env.COINMARKET_API_KEY || '';
+
+// const HARDHAT_CHAIN_ID = Number(process.env.HARDHAT_CHAIN_ID) || 31337
+// const SEPOLIA_CHAIN_ID = Number(process.env.SEPOLIA_CHAIN_ID) || 11155111
+// const LOCALHOST_CHAIN_ID = Number(process.env.LOCALHOST_CHAIN_ID) || 1337
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: '0.8.24',
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      blockConfirmations: 1,
+    },
+    localhost: {
+      chainId: 31337,
+      blockConfirmations: 1,
+    },
+    sepolia: {
+      chainId: 11155111,
+      accounts: [PRIVATE_KEY],
+      url: SEPOLIA_RPC_URL,
+      blockConfirmations: 6,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+    customChains: [
+      {
+        network: 'sepolia',
+        chainId: 11155111,
+        urls: {
+          apiURL: 'https://api-sepolia.etherscan.io/api',
+          browserURL: 'https://sepolia.etherscan.io',
+        },
+      },
+    ],
+  },
+  gasReporter: {
+    enabled: false,
+    currency: 'INR',
+    outputFile: 'gas-report.txt',
+    noColors: true,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    player: {
+      default: 1,
+    },
+  },
+  mocha: {
+    timeout: 300000, // 300000 milisecond = 300 seconds
+  },
+};
