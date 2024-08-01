dfde81dad6cb769a423094a1628b2381d1ba4a93
Manajit Halder <manajit.halder@gmail.com>
2024-08-01 10:07:43 +0530

    Added code for uploading image to pinata and changed deploy scripts accordingly

--- freecodecamp/hardhat-nft-fcc/deploy/02-deploy-random-ipfs-nft.js
+++ freecodecamp/hardhat-nft-fcc/deploy/02-deploy-random-ipfs-nft.js
@@ -1,9 +1,23 @@
-const { network } = require("hardhat")
+const { network, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../helper.hardhat.config")
 const { verify } = require("../utils/verify")
-const { storeImages } = require("../utils/uploadToPinata")
+const {
+    storeImages,
+    storeTokenUriMetadata,
+} = require("../utils/uploadToPinata")
 
 const imagesLocation = "./images/randomNft"
+const metadataTemplate = {
+    name: "",
+    description: "",
+    image: "",
+    attributes: [
+        {
+            trait_type: "cuteness",
+            value: 100,
+        },
+    ],
+}
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
@@ -11,21 +25,23 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
     const chainId = network.config.chainId
 
     // get the IPFS hashes of our images
-
     // 1. With our own IPFS nodel. https: //docs. ipfs. io/
     // 2. pinata ht bs: //www.pinata.cloud/
     // 3. nft.storage https://nft.storage/
-    let tokenURIs
-    if (process.env.UPLOAD_TO_PINATA) {
+    let tokenURIs = [
+        "ipfs://QmQs4yASJakykKzcUYiJoQEFptCuufghNA3S5J2CkD47tp",
+        "ipfs://QmXry9jwWVKfbt6V87Gzd97WJ5LGAmtyWY7znSQXCRysv9",
+        "ipfs://QmX5V7Xc31vMfM8tYgrNefix1WCFmiMqpLzjDtk6PgTQd2",
+    ]
+
+    if (process.env.UPLOAD_TO_PINATA == "true") {
         tokenURIs = await handleTokenURIs()
     }
 
-    let vrfCoordinatorV2Address, subscriptionId
+    let vrfCoordinatorV2Address, subscriptionId, vrfCoordinatorV2Mock
 
     if (developmentChains.includes(network.name)) {
-        const vrfCoordinatorV2Mock = await ethers.getContract(
-            "VRFCoordinatorV2Mock"
-        )
+        vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
         vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
         const tx = await vrfCoordinatorV2Mock.createSubscription()
         const txReceipt = await tx.wait(1)
@@ -37,30 +53,29 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
 
     log("--------------------------------------------------------------------")
 
-    await storeImages(imagesLocation)
+    const args = [
+        vrfCoordinatorV2Address,
+        subscriptionId,
+        networkConfig[chainId].keyHash,
+        networkConfig[chainId].callbackGasLimit,
+        tokenURIs,
+        networkConfig[chainId].mintFee,
+        vrfCoordinatorV2Address,
+    ]
 
-    // const args = [
-    //     vrfCoordinatorV2Address,
-    //     subscriptionId,
-    //     networkConfig[chainId].keyHash,
-    //     networkConfig[chainId].callbackGasLimit,
-    //     //dogTokenURIs
-    //     networkConfig[chainId].mintFee,
-    //     //initialOwner
-    // ]
-    // const randomIpfsNft = await deploy("RandomIpfsNft", {
-    //     from: deployer,
-    //     args: args,
-    //     log: true,
-    //     waitConfirmations: network.config.blockConfirmations || 1,
-    // })
+    const randomIpfsNft = await deploy("RandomIpfsNft", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
 
     // Verify the deployment
     if (
         !developmentChains.includes(network.name) &&
         process.env.ETHERSCAN_API_KEY
     ) {
-        log("Verifying BasicNft ...")
+        log("Verifying RandomIpfsNft ...")
         await verify(randomIpfsNft.address, args)
     }
 
@@ -71,6 +86,29 @@ async function handleTokenURIs() {
     tokenURIs = []
     // 1. store images to IPFS
     // 2. store metadata to IPFS
+    const { responses: imageUploadResponses, files } = await storeImages(
+        imagesLocation
+    )
+    for (imageUploadResponseIndex in imageUploadResponses) {
+        // create metadata
+        // upload the metadata
+        let tokenUriMetadata = { ...metadataTemplate }
+        tokenUriMetadata.name = files[imageUploadResponseIndex].replace(
+            ".png",
+            ""
+        )
+        tokenUriMetadata.description = `An adorable ${tokenUriMetadata.name} pup!`
+        tokenUriMetadata.image = `ipfs://${imageUploadResponses[imageUploadResponseIndex].IpfsHash}`
+        console.log("Uploading ", tokenUriMetadata.name)
+
+        // Store the JSON to pinata / IPFS
+        const metadataUploadResponse = await storeTokenUriMetadata(
+            tokenUriMetadata
+        )
+        tokenURIs.push(`ipfs://${metadataUploadResponse.IpfsHash}`)
+    }
+    console.log("Uploaded Token URIs are!!!")
+    console.log(tokenURIs)
 
     return tokenURIs
 }
--- freecodecamp/hardhat-nft-fcc/utils/uploadToPinata.js
+++ freecodecamp/hardhat-nft-fcc/utils/uploadToPinata.js
@@ -1,11 +1,48 @@
 const pinataSDK = require("@pinata/sdk")
 const path = require("path")
 const fs = require("fs")
+require("dotenv").config()
+
+const pinataApiKey = process.env.PINATA_API_KEY
+const pinataApiSecret = process.env.PINATA_API_SECRET
+const pinata = pinataSDK(pinataApiKey, pinataApiSecret)
 
 async function storeImages(imageFilePath) {
+    console.log("Uploading to Pinata!!!")
+
     const fullImagePath = path.resolve(imageFilePath)
     const files = fs.readdirSync(fullImagePath)
-    console.log(files)
+
+    console.log(`\nfullImagePath: ${fullImagePath}`)
+    console.log(`\nfiles: ${files}`)
+
+    let responses = []
+    for (let fileIndex in files) {
+        console.log(`Working on file ${fileIndex}`)
+        const readableStreamForFiles = fs.createReadStream(
+            `${fullImagePath}/${files[fileIndex]}`
+        )
+        try {
+            const response = await pinata.pinFileToIPFS(readableStreamForFiles)
+            responses.push(response)
+        } catch (error) {
+            console.error(error)
+        }
+        console.log("\nreadableStreamForFiles: ", readableStreamForFiles.path)
+    }
+
+    return { responses, files }
 }
 
-module.exports = { storeImages }
+async function storeTokenUriMetadata(metadata) {
+    try {
+        const response = await pinata.pinJSONToIPFS(metadata)
+        return response
+    } catch (error) {
+        console.log(error)
+    }
+
+    return null
+}
+
+module.exports = { storeImages, storeTokenUriMetadata }
